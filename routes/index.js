4bdd1081b857b764a3d4f448347e683699f4d404
unknown <abhishek.ma@nichi.com>
2021-02-01 12:13:16 +0530

    test2 commit

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });  // test1 commit
-};
+  res.render('index', { title: 'Express' });  // test1 commit 
+};  // and test2 commit
